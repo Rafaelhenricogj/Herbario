ff2516bd7a0fe672dc405772186f5c96abb980f3
Rafael Henrico <rafaelhenrico18@gmail.com>
2023-10-30 12:17:59 -0300

    Add Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  {path: '', component: MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
